cb22a7a91ff3ede06e659d1ae7fb25d229ffa57d
Mariam Omara <mariamomara2005@gmail.com>
2026-09-17 23:15:14 +0300

    Q5 PRACTICAL

--- OOPASS-03/Program.cs
+++ OOPASS-03/Program.cs
@@ -1,6 +1,283 @@
 ﻿//namespace OOPASS_03
 //{
 //    using System;
+//    //Q5 PRACTICAL
+//    public class Shipment
+//    {
+//        public string TrackingCode { get; set; }
+//        public string Description { get; set; }
+//        public double Weight { get; set; }
+//        public decimal DeliveryFee { get; set; }
+
+//        public virtual decimal EstimatedCost
+//        {
+//            get
+//            {
+//                return DeliveryFee + ((decimal)Weight * 5);
+//            }
+//        }
+
+//        public virtual void PrintShipment()
+//        {
+//            Console.WriteLine($"Tracking Code: {TrackingCode}");
+//            Console.WriteLine($"Description: {Description}");
+//            Console.WriteLine($"Weight: {Weight} KG");
+//            Console.WriteLine($"Delivery Fee: {DeliveryFee} EGP");
+//            Console.WriteLine($"Estimated Cost: {EstimatedCost} EGP");
+//        }
+
+//        public Shipment(
+//            string trackingCode,
+//            string description,
+//            double weight,
+//            decimal deliveryFee)
+//        {
+//            TrackingCode = trackingCode;
+//            Description = description;
+//            Weight = weight;
+//            DeliveryFee = deliveryFee;
+//        }
+//    }
+
+//    public class StandardShipment : Shipment
+//    {
+//        public StandardShipment(
+//            string trackingCode,
+//            string description,
+//            double weight,
+//            decimal deliveryFee)
+//            : base(trackingCode, description, weight, deliveryFee)
+//        {
+//        }
+
+//        public override void PrintShipment()
+//        {
+//            Console.WriteLine("=== Standard Shipment ===");
+//            Console.WriteLine($"Tracking Code: {TrackingCode}");
+//            Console.WriteLine($"Description: {Description}");
+//            Console.WriteLine($"Weight: {Weight} KG");
+//            Console.WriteLine($"Delivery Fee: {DeliveryFee} EGP");
+//            Console.WriteLine($"Estimated Cost: {EstimatedCost} EGP");
+//        }
+//    }
+
+//    public class ExpressShipment : Shipment
+//    {
+//        public decimal ExtraFee { get; set; }
+
+//        public ExpressShipment(
+//            string trackingCode,
+//            string description,
+//            double weight,
+//            decimal deliveryFee,
+//            decimal extraFee)
+//            : base(trackingCode, description, weight, deliveryFee)
+//        {
+//            ExtraFee = extraFee;
+//        }
+
+//        public override decimal EstimatedCost
+//        {
+//            get
+//            {
+//                return DeliveryFee + ((decimal)Weight * 5) + ExtraFee;
+//            }
+//        }
+
+//        public override void PrintShipment()
+//        {
+//            Console.WriteLine("=== Express Shipment ===");
+//            Console.WriteLine($"Tracking Code: {TrackingCode}");
+//            Console.WriteLine($"Description: {Description}");
+//            Console.WriteLine($"Weight: {Weight} KG");
+//            Console.WriteLine($"Delivery Fee: {DeliveryFee} EGP");
+//            Console.WriteLine($"Extra Fee: {ExtraFee} EGP");
+//            Console.WriteLine($"Estimated Cost: {EstimatedCost} EGP");
+//        }
+//    }
+
+//    public class InternationalShipment : Shipment
+//    {
+//        public string DestinationCountry { get; set; }
+//        public decimal CustomsFee { get; set; }
+
+//        public InternationalShipment(
+//            string trackingCode,
+//            string description,
+//            double weight,
+//            decimal deliveryFee,
+//            string destinationCountry,
+//            decimal customsFee)
+//            : base(trackingCode, description, weight, deliveryFee)
+//        {
+//            DestinationCountry = destinationCountry;
+//            CustomsFee = customsFee;
+//        }
+
+//        public override decimal EstimatedCost
+//        {
+//            get
+//            {
+//                return DeliveryFee + ((decimal)Weight * 5) + CustomsFee;
+//            }
+//        }
+
+//        public override void PrintShipment()
+//        {
+//            Console.WriteLine("=== International Shipment ===");
+//            Console.WriteLine($"Tracking Code: {TrackingCode}");
+//            Console.WriteLine($"Description: {Description}");
+//            Console.WriteLine($"Weight: {Weight} KG");
+//            Console.WriteLine($"Delivery Fee: {DeliveryFee} EGP");
+//            Console.WriteLine($"Destination Country: {DestinationCountry}");
+//            Console.WriteLine($"Customs Fee: {CustomsFee} EGP");
+//            Console.WriteLine($"Estimated Cost: {EstimatedCost} EGP");
+//        }
+//    }
+
+//    public class DeliveryCenter
+//    {
+//        private Shipment[] _shipments;
+
+//        public DeliveryCenter(int capacity)
+//        {
+//            _shipments = new Shipment[capacity];
+//        }
+
+//        // Integer Indexer
+//        public Shipment this[int index]
+//        {
+//            get
+//            {
+//                if (index >= 0 && index < _shipments.Length)
+//                {
+//                    return _shipments[index];
+//                }
+
+//                return null;
+//            }
+
+//            set
+//            {
+//                if (index >= 0 && index < _shipments.Length)
+//                {
+//                    _shipments[index] = value;
+//                }
+//            }
+//        }
+
+//        // String Indexer
+//        public Shipment this[string trackingCode]
+//        {
+//            get
+//            {
+//                for (int i = 0; i < _shipments.Length; i++)
+//                {
+//                    if (_shipments[i] != null &&
+//                        _shipments[i].TrackingCode == trackingCode)
+//                    {
+//                        return _shipments[i];
+//                    }
+//                }
+
+//                return null;
+//            }
+//        }
+
+//        public bool AddShipment(Shipment shipment)
+//        {
+//            for (int i = 0; i < _shipments.Length; i++)
+//            {
+//                if (_shipments[i] == null)
+//                {
+//                    _shipments[i] = shipment;
+//                    return true;
+//                }
+//            }
+
+//            return false;
+//        }
+
+//        public bool RemoveShipment(string trackingCode)
+//        {
+//            for (int i = 0; i < _shipments.Length; i++)
+//            {
+//                if (_shipments[i] != null &&
+//                    _shipments[i].TrackingCode == trackingCode)
+//                {
+//                    _shipments[i] = null;
+//                    return true;
+//                }
+//            }
+
+//            return false;
+//        }
+
+//        // Q5
+//        public void PrintAllShipments()
+//        {
+//            for (int i = 0; i < _shipments.Length; i++)
+//            {
+//                if (_shipments[i] != null)
+//                {
+//                    _shipments[i].PrintShipment();
+//                    Console.WriteLine();
+//                }
+//            }
+//        }
+//    }
+
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            DeliveryCenter center = new DeliveryCenter(5);
+
+//            StandardShipment standard =
+//                new StandardShipment(
+//                    "SH001",
+//                    "Laptop",
+//                    3,
+//                    50);
+
+//            ExpressShipment express =
+//                new ExpressShipment(
+//                    "SH002",
+//                    "Mobile",
+//                    2,
+//                    50,
+//                    30);
+
+//            InternationalShipment international =
+//                new InternationalShipment(
+//                    "SH003",
+//                    "TV",
+//                    8,
+//                    100,
+//                    "USA",
+//                    150);
+
+//            center.AddShipment(standard);
+//            center.AddShipment(express);
+//            center.AddShipment(international);
+
+//            center.PrintAllShipments();
+//        }
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+//    using System;
 //    //Q4 PRACTICAL
 //    public class Shipment
 //    {
