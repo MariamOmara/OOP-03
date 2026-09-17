b01154b10448c5b3cec7dd0911878a6d8b11a801
Mariam Omara <mariamomara2005@gmail.com>
2026-09-17 22:31:22 +0300

    Q1 PRACTICAL

--- OOPASS-03/Program.cs
+++ OOPASS-03/Program.cs
@@ -1,13 +1,265 @@
-﻿namespace OOPASS_03
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿//namespace OOPASS_03
+//{
+//    using System;
+//    // Q1 PRACTICAL
+//    public struct DeliveryAddress
+//    {
+//        public string City;
+//        public string Street;
+//        public int BuildingNumber;
+
+//        public DeliveryAddress(string city, string street, int buildingNumber)
+//        {
+//            City = city;
+//            Street = street;
+//            BuildingNumber = buildingNumber;
+//        }
+
+//        public string GetFullAddress()
+//        {
+//            return $"{BuildingNumber} {Street}, {City}";
+//        }
+//    }
+
+//    public class Shipment
+//    {
+//        private string _trackingCode;
+//        private string _description;
+//        private double _weight;
+//        private decimal _deliveryFee;
+
+//        public string TrackingCode
+//        {
+//            get
+//            {
+//                return _trackingCode;
+//            }
+//        }
+
+//        public string Description
+//        {
+//            get
+//            {
+//                return _description;
+//            }
+//            set
+//            {
+//                if (!string.IsNullOrWhiteSpace(value))
+//                {
+//                    _description = value;
+//                }
+//            }
+//        }
+
+//        public double Weight
+//        {
+//            get
+//            {
+//                return _weight;
+//            }
+//            set
+//            {
+//                if (value > 0)
+//                {
+//                    _weight = value;
+//                }
+//            }
+//        }
+
+//        public decimal DeliveryFee
+//        {
+//            get
+//            {
+//                return _deliveryFee;
+//            }
+//            private set
+//            {
+//                if (value > 0)
+//                {
+//                    _deliveryFee = value;
+//                }
+//            }
+//        }
+
+//        public DeliveryAddress Destination { get; set; }
+
+//        // Point 1: EstimatedCost is virtual
+//        public virtual decimal EstimatedCost
+//        {
+//            get
+//            {
+//                return DeliveryFee + ((decimal)Weight * 5);
+//            }
+//        }
+
+//        public Shipment(
+//            string trackingCode,
+//            string description,
+//            double weight,
+//            decimal deliveryFee,
+//            DeliveryAddress destination)
+//        {
+//            _trackingCode =
+//                string.IsNullOrWhiteSpace(trackingCode)
+//                ? "Unknown"
+//                : trackingCode;
+
+//            _description =
+//                string.IsNullOrWhiteSpace(description)
+//                ? "Unknown"
+//                : description;
+
+//            _weight =
+//                weight > 0
+//                ? weight
+//                : 1;
+
+//            _deliveryFee =
+//                deliveryFee > 0
+//                ? deliveryFee
+//                : 50;
+
+//            Destination = destination;
+//        }
+
+//        public void UpdateDeliveryFee(decimal newFee)
+//        {
+//            if (newFee > 0)
+//            {
+//                DeliveryFee = newFee;
+//            }
+//        }
+
+//        // Method Overloading - Version 1
+//        public void UpdateWeight(double newWeight)
+//        {
+//            if (newWeight > 0)
+//            {
+//                Weight = newWeight;
+//            }
+//        }
+
+//        // Method Overloading - Version 2
+//        public void UpdateWeight(double newWeight, double extraPackingWeight)
+//        {
+//            double totalWeight = newWeight + extraPackingWeight;
+
+//            if (totalWeight > 0)
+//            {
+//                Weight = totalWeight;
+//            }
+//        }
+
+//        // Point 2: PrintShipment is virtual
+//        public virtual void PrintShipment()
+//        {
+//            Console.WriteLine($"Tracking Code: {TrackingCode}");
+//            Console.WriteLine($"Description: {Description}");
+//            Console.WriteLine($"Weight: {Weight} KG");
+//            Console.WriteLine($"Delivery Fee: {DeliveryFee} EGP");
+//            Console.WriteLine($"Destination: {Destination.GetFullAddress()}");
+//            Console.WriteLine($"Estimated Cost: {EstimatedCost} EGP");
+//        }
+//    }
+
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            DeliveryAddress address =
+//                new DeliveryAddress("Cairo", "Tahrir Street", 15);
+
+//            Shipment shipment =
+//                new Shipment(
+//                    "SH001",
+//                    "Laptop",
+//                    3,
+//                    80,
+//                    address);
+
+//            shipment.PrintShipment();
+
+//            Console.WriteLine();
+
+//            shipment.UpdateWeight(5);
+//            Console.WriteLine($"Updated Weight: {shipment.Weight} KG");
+
+//            shipment.UpdateWeight(5, 0.5);
+//            Console.WriteLine(
+//                $"Updated Weight After Packing: {shipment.Weight} KG");
+//        }
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // النظري
 //Q1  A
 
